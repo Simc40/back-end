563cb527cfc6b5be1580038f864355711081b4c0
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2023-01-16 13:38:04 -0300

    edited planning

--- config/cors.js
+++ config/cors.js
@@ -1,9 +1,9 @@
 //Configure Cross-Origin Resource Sharing (CORS)
 const corsOptions = {
-    // origin: '*',
-    origin: ['http://localhost:8000','http://localhost:8001','http://192.168.0.2:8000'], 
-    credentials:true,                     //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+  origin: "*",
+  // origin: ['http://localhost:8000','http://localhost:8001','http://192.168.0.2:8000'],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 exports.corsOptions = corsOptions;
