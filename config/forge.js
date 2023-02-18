f2ecfead4bb9de081fdf4a6c15b2189cdeb9f96f
Julio Cesar Lopes <58558069+julioclopes32@users.noreply.github.com>
2023-02-18 12:26:28 -0300

    update

--- config/forge.js
+++ config/forge.js
@@ -1,5 +1,7 @@
-const APS_CLIENT_ID = "OkQ3U2vGDrfXIRbZWPSNHkpornRX9jnA"
-const APS_CLIENT_SECRET = "8IOAvUiwCh4a9zGG"
+// const APS_CLIENT_ID = "OkQ3U2vGDrfXIRbZWPSNHkpornRX9jnA"
+// const APS_CLIENT_SECRET = "8IOAvUiwCh4a9zGG"
+const APS_CLIENT_ID = "8nO7TjEkuz6GA3Yc77sdGV5Zk0qEvMNJ";
+const APS_CLIENT_SECRET = "lqZcPUEi7H6XJygl"
 const APS_BUCKET = `${APS_CLIENT_ID.toLowerCase()}-basic-app`;
 
 module.exports = {
--- routes/pecas.js
+++ routes/pecas.js
@@ -1,5 +1,6 @@
 
 const admin = require("firebase-admin");                              
+const { uploadInRealTimeDatabase, setInRealTimeDatabase } = require('../models/firebaseDatabase');
 
 module.exports = function (app, sessions_map, get_database) {
 
@@ -13,5 +14,59 @@ module.exports = function (app, sessions_map, get_database) {
             console.log('The read failed: ' + errorObject.name);
             res.status(507).send()
         });
+    });
+
+    app.post('/qrcode', (req, res) => {
+        const new_db = get_database(sessions_map.get(req.sessionID).cliente.database)
+        let ref = admin.database(new_db).ref(`pecas`);
+        let obra;
+        let promises = [];
+        // console.log(req.body.params)
+        let elements = {};
+        Object.values(req.body.params).forEach((child) => {
+            obra = child.element.obra;
+            let p = new Promise((resolve, reject) => {
+                uploadInRealTimeDatabase(new_db, `pecas/${child.element.obra}/${child.element.uid}/${child.form.uid}`, child.form, false)
+                .then(() => {
+                    elements[child.element.uid] = elements[child.element.uid] === undefined ? parseInt(child.element.numPecas) + 1 : elements[child.element.uid] + 1;
+                })
+                .then(resolve)
+                .catch((e) => {
+                    reject(e)
+                })
+            })
+            promises.push(p);
+        });
+
+        const elementPromisesFunction = () => {
+            let elementsPromises = []
+            Object.entries(elements).forEach((element) => {
+                let elemProm = new Promise((resolve, reject) => {
+                    setInRealTimeDatabase(new_db, `elementos/${obra}/${element[0]}/numPecas`, element[1].toString(), false)
+                    .then(resolve)
+                    .catch((e) => {
+                        reject(e)
+                    })
+                })
+                elementsPromises.push(elemProm);
+            });
+            return elementsPromises;
+        }
+
+        Promise.all(promises)
+        .then(elementPromisesFunction)
+        .then((elementPromises) => {
+            Promise.all(elementPromises)
+            .then(() => {
+                return;
+            })
+        })
+        .then(() => {
+            res.status(200).send();
+        })
+        .catch((e) => {
+            console.log(e);
+            res.status(507).send();
+        })
     });
 }
